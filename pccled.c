0d340930a74044e4eac95daa0d98b818437a7e3e
MCLT-YM <y.matsumoto@morijyobi.ac.jp>
2020-11-24 23:13:22 +0900

    LEDファイルの追加

---
+++ pccled.c
@@ -0,0 +1,18 @@
+﻿#include "iodefine.h"
+#include "pccled.h"
+
+void init_led(void)
+{
+    PORTB.PDR.BIT.B0 = 1;
+    PORTA.PDR.BIT.B6 = 1;
+    PORTA.PDR.BIT.B4 = 1;
+    PORTA.PDR.BIT.B0 = 1;
+}
+
+void set_led(unsigned char data)
+{
+    PORTB.PODR.BIT.B0 = data & 0x01;
+    PORTA.PODR.BIT.B6 = (data>>1) & 0x01;
+    PORTA.PODR.BIT.B4 = (data>>2) & 0x01;
+    PORTA.PODR.BIT.B0 = (data>>3) & 0x01;
+}
---
+++ pccled.h
@@ -0,0 +1,4 @@
+﻿#ifndef PCC_LED_H
+void init_led(void);
+void set_led(unsigned char data);
+#endif
